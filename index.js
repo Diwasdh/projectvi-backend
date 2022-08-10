0cdea213642c5afd9dfc03d06405b9ccb0d6f45a
Diwas Dhakal <diwasdhakal75@gmail.com>
2022-08-10 17:12:44 +0545

    asd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+let port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,6 +14,6 @@ app.use("/v1/api/users", require("./routes/api/users"));
 app.use("/v1/api/auth", require("./routes/api/auth"));
 
 //App Listen
-app.listen(5000, function () {
-   console.log("Example app listening on port 5000!");
+app.listen(port, function () {
+   console.log(`Example app listening on port `);
 });
