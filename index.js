799346b76b2e0b4359c447189a762a2271395d36
Diwas Dhakal <diwasdhakal75@gmail.com>
2022-08-10 17:17:08 +0545

    Change

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var app = express();
-let port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +13,6 @@ app.use("/v1/api/users", require("./routes/api/users"));
 app.use("/v1/api/auth", require("./routes/api/auth"));
 
 //App Listen
-app.listen(port, function () {
+app.listen(3000, function () {
    console.log(`Example app listening on port `);
 });
